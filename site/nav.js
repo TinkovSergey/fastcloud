d55e468dbd172fdaecd5ccd2d5cdd3e67a1debf2
TinkovSergey <tinkov.sergey@gmail.com>
2026-05-21 13:51:49 +0400

    Очищен кэш Git

--- site/nav.js
+++
@@ -1,9 +0,0 @@
-(function () {
-  var nav = document.querySelector('.mkt-nav');
-  if (!nav) return;
-  function update() {
-    nav.classList.toggle('scrolled', window.scrollY > 10);
-  }
-  window.addEventListener('scroll', update, { passive: true });
-  update();
-})();
